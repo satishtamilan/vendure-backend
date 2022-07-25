23022e364d1781efc9445bf6a65ad46bb17fe33f
Satish Kumar Anandhan <iamsatish@live.com>
2022-07-25 07:55:45 +0100

    Update vendure-config.ts

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -11,7 +11,7 @@ import path from 'path';
 
 export const config: VendureConfig = {
     apiOptions: {
-        port: 3000,
+        port: 443,
         adminApiPath: 'admin-api',
         adminApiPlayground: {
             settings: {
