c789eff68276bbc3df20494e49c5c1490e54d8ad
Satish Kumar Anandhan <iamsatish@live.com>
2022-08-23 22:06:17 +0100

    Update vendure-config.ts

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -1,8 +1,8 @@
 import {
-    dummyPaymentHandler,
-    DefaultJobQueuePlugin,
-    DefaultSearchPlugin,
-    VendureConfig,
+  dummyPaymentHandler,
+  DefaultJobQueuePlugin,
+  DefaultSearchPlugin,
+  VendureConfig,
 } from '@vendure/core'; 
 import { defaultEmailHandlers, EmailPlugin } from '@vendure/email-plugin';
 import { AssetServerPlugin } from '@vendure/asset-server-plugin';
@@ -10,72 +10,84 @@ import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
 import path from 'path';
 
 export const config: VendureConfig = {
-    apiOptions: {
-        port: 443,
-        adminApiPath: 'admin-api',
-        adminApiPlayground: {
-            settings: {
-                'request.credentials': 'include',
-            } as any,
-        },// turn this off for production
-        adminApiDebug: true, // turn this off for production
-        shopApiPath: 'shop-api',
-        shopApiPlayground: {
-            settings: {
-                'request.credentials': 'include',
-            } as any,
-        },// turn this off for production
-        shopApiDebug: true,// turn this off for production
-    },
-    authOptions: {
-        superadminCredentials: {
-            identifier: 'superadmin',
-            password: 'superadmin',
+  
+  apiOptions: {
+      port: 3000,
+      adminApiPath: 'admin-api',
+      adminApiPlayground: {
+          settings: {
+              'request.credentials': 'include',
+          } as any,
+      },// turn this off for production
+      adminApiDebug: true, // turn this off for production
+      shopApiPath: 'shop-api',
+      shopApiPlayground: {
+          settings: {
+              'request.credentials': 'include',
+          } as any,
+      },// turn this off for production
+      shopApiDebug: true,// turn this off for production
+  },
+  authOptions: {
+     tokenMethod:   'bearer',     
+      superadminCredentials: {
+          identifier: 'superadmin',
+          password: 'superadmin',
+      },
+      cookieOptions: {
+        secret: process.env.COOKIE_SECRET || 'cookie-secret',
+      },
+  },
+  dbConnectionOptions: {
+      type: 'postgres',
+      synchronize: true, // turn this off for production
+      logging: false,
+      database: 'satishlowes.vendure',
+      host: 'db.bit.io',
+      port: 5432,
+      username: 'satishlowes',
+      password: 'v2_3sfVb_ntb5Yzrt4M3G5DwsKfx8PQd',
+      ssl: true,
+      migrations: [path.join(__dirname, '../migrations/*.ts')],
+  },
+  paymentOptions: {
+      paymentMethodHandlers: [dummyPaymentHandler],
+  },
+  customFields: {},
+  plugins: [
+      AssetServerPlugin.init({
+          route: 'assets',
+          assetUploadDir: path.join(__dirname, '../static/assets'),
+      }),
+      DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
+      DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
+      EmailPlugin.init({
+        transport: {
+          type: 'smtp',
+          host: 'smtp.mandrillapp.com',
+          port: 587,
+          auth: {
+            user: 'kaaikani',
+            pass: 'pK13fnoG9DJbCQqX3sDkLg',
+          }
         },
-        cookieOptions: {
-          secret: process.env.COOKIE_SECRET || 'cookie-secret',
-        },
-    },
-    dbConnectionOptions: {
-        type: 'postgres',
-        synchronize: true, // turn this off for production
-        logging: false,
-        database: 'satishlowes.vendure',
-        host: 'db.bit.io',
-        port: 5432,
-        username: 'satishlowes',
-        password: 'v2_3sfVb_ntb5Yzrt4M3G5DwsKfx8PQd',
-        ssl: true,
-        migrations: [path.join(__dirname, '../migrations/*.ts')],
-    },
-    paymentOptions: {
-        paymentMethodHandlers: [dummyPaymentHandler],
-    },
-    customFields: {},
-    plugins: [
-        AssetServerPlugin.init({
-            route: 'assets',
-            assetUploadDir: path.join(__dirname, '../static/assets'),
-        }),
-        DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
-        DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
-        EmailPlugin.init({
-            devMode: true,
-            outputPath: path.join(__dirname, '../static/email/test-emails'),
-            route: 'mailbox',
-            handlers: defaultEmailHandlers,
-            templatePath: path.join(__dirname, '../static/email/templates'),
-            globalTemplateVars: {
-                // The following variables will change depending on your storefront implementation
-                fromAddress: '"example" <noreply@example.com>',
-                verifyEmailAddressUrl: 'http://localhost:8080/verify',
-                passwordResetUrl: 'http://localhost:8080/password-reset',
-                changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change'
-            },
-        }),
-        AdminUiPlugin.init({
-            route: 'admin',
-            port: 3002,
-        }),
-    ],
+          devMode: true,
+          outputPath: path.join(__dirname, '../static/email/test-emails'),
+          route: 'mailbox',
+          handlers: defaultEmailHandlers,
+          templatePath: path.join(__dirname, '../static/email/templates'),
+          globalTemplateVars: {
+              // The following variables will change depending on your storefront implementation
+              fromAddress: '"example" <noreply@example.com>',
+              verifyEmailAddressUrl: 'http://localhost:8080/verify',
+              passwordResetUrl: 'http://localhost:8080/password-reset',
+              changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change'
+          },
+      }),
+      AdminUiPlugin.init({
+          route: 'admin',
+          port: 3002,
+      }),
+  ],
+  
 };
